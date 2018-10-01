e164d44842e62cdf292a5d7f558795f7d61612e5
sorianog <13261711+sorianog@users.noreply.github.com>
2018-09-30 22:37:49 -0400

    Complete setup of ARCore Sceneform config

--- app/src/main/java/com/way/cloudnine/wherearyou/MainActivity.java
+++ app/src/main/java/com/way/cloudnine/wherearyou/MainActivity.java
@@ -2,12 +2,12 @@ package com.way.cloudnine.wherearyou;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import org.jetbrains.annotations.Nullable;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
     }
